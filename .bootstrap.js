6ec3acdc8745f5205a28867feb400a72f3540b55
Vibesdk <vibesdk-bot@cloudflare.com>
2025-12-11 08:14:53 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add react-markdown remark-gfm xlsx"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
